0b3adcefab7cf1e7967a0ce56ef0b80fb4f583d5
Екатерина <erusetskaya@gmail.com>
2020-02-25 02:28:02 +0300

    реализованы классы-помощники для контактов и групп

--- fixtura/application.py
+++ fixtura/application.py
@@ -4,6 +4,7 @@
 from selenium import webdriver
 from fixtura.session import SessionHelper
 from fixtura.group import GroupHelper
+from fixtura.contact import ContactHelper
 
 class Application:
 
@@ -12,6 +13,7 @@ class Application:
         self.wd.implicitly_wait(60)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
+        self.contact = ContactHelper(self)
 
     def open_home_page(self):
         wd = self.wd
@@ -21,43 +23,6 @@ class Application:
         wd = self.wd
         wd.find_element_by_link_text("home page").click()
 
-    def create_new_contact(self, contact):
-        wd = self.wd
-        # create new contact
-        wd.find_element_by_link_text("add new").click()
-        #fill contact fields (displayd on home page)
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.first_name)
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.last_name)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(contact.address)
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(contact.home_phone_number)
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(contact.mobile_phone_number)
-        wd.find_element_by_name("work").click()
-        wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys(contact.work_phone_number)
-        wd.find_element_by_name("fax").click()
-        wd.find_element_by_name("fax").clear()
-        wd.find_element_by_name("fax").send_keys(contact.fax_number)
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(contact.email_1)
-        wd.find_element_by_name("email2").click()
-        wd.find_element_by_name("email2").clear()
-        wd.find_element_by_name("email2").send_keys(contact.email_2)
-        wd.find_element_by_name("email3").click()
-        wd.find_element_by_name("email3").clear()
-        wd.find_element_by_name("email3").send_keys(contact.email_3)
-        # submit contact creation
-        wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
-        self.return_to_home_page()
 
     def destroy(self):
         self.wd.quit()
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -11,22 +11,22 @@ def app(request):
     return fixture
 
 def test_add_contact(app):
-    app.login(username="admin", password="secret")
-    app.create_new_contact(Contact(first_name="testFirstName", last_name="TestLastName",
-                                        address="jgjvhzjvjzvjv 887878", home_phone_number="765787657888",
-                            mobile_phone_number="88599485", work_phone_number="886945694565476",
-                                        fax_number="84584758", email_1="dgkjgjgkdgk", email_2="kjhfkdhkhf",
-                            email_3="kjxkjxlhj"))
-    app.logout()
+    app.session.login(username="admin", password="secret")
+    app.contact.create_new(Contact(first_name="testFirstName", last_name="TestLastName",
+                                   address="jgjvhzjvjzvjv 887878", home_phone_number="765787657888",
+                                   mobile_phone_number="88599485", work_phone_number="886945694565476",
+                                   fax_number="84584758", email_1="dgkjgjgkdgk", email_2="kjhfkdhkhf",
+                                   email_3="kjxkjxlhj"))
+    app.session.logout()
 
 def test_add_empty_contact(app):
-    app.login(username="admin", password="secret")
-    app.create_new_contact(Contact(first_name="", last_name="",
-                                        address="", home_phone_number="",
-                            mobile_phone_number="", work_phone_number="",
-                                        fax_number="", email_1="", email_2="",
-                            email_3=""))
-    app.logout()
+    app.session.login(username="admin", password="secret")
+    app.contact.create_new(Contact(first_name="", last_name="",
+                                   address="", home_phone_number="",
+                                   mobile_phone_number="", work_phone_number="",
+                                   fax_number="", email_1="", email_2="",
+                                   email_3=""))
+    app.session.logout()
 
 if __name__ == "__main__":
     unittest.main()
