3dadb9b4069271f6ff337ea07c85aac23789cc46
Екатерина <erusetskaya@gmail.com>
2020-02-24 19:37:59 +0300

    Выделен класс SessionHelper (исправлена синтаксическая ошибка)

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -16,7 +16,7 @@ def test_add_group(app):
     app.session.logout()
 
 def test_add_empty_group(app):
-    app.session.ogin(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_group(Group(name="", header="", footer=""))
     app.session.logout()
 
