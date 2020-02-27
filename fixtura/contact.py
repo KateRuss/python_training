2a59dda8170a35e2ca4ae022d88cb9071439441d
Екатерина <erusetskaya@gmail.com>
2020-02-28 00:40:43 +0300

    реализован тест модификации контакта. Последние правки

--- fixtura/contact.py
+++ fixtura/contact.py
@@ -2,7 +2,6 @@
 class ContactHelper:
     def __init__(self, app):
         self.app = app
-        self.accept_next_alert = False
 
     def create_new(self, contact):
         wd = self.app.wd
@@ -44,27 +43,53 @@ class ContactHelper:
 
     def delete_first_contact(self):
         wd = self.app.wd
-        self.accept_next_alert = True  # <- for control alert window
         # select first contact
         wd.find_element_by_name("selected[]").click()
         # delete element
         wd.find_element_by_xpath("//input[@value='Delete']").click()
         # submit element delete
-        self.close_alert()
+        wd.switch_to.alert.accept()
 
     def delete_all_contact(self):
         wd = self.app.wd
-        self.accept_next_alert = True  # <- for control alert window
         wd.find_element_by_id("MassCB").click()
         wd.find_element_by_xpath("//input[@value='Delete']").click()
-        # submit element delete
-        self.close_alert()
+        # submit contact delete
+        wd.switch_to.alert.accept()
 
-
-    def close_alert(self):
+    def edit_first_contact(self, contact):
         wd = self.app.wd
-        alert = wd.switch_to.alert
-        if self.accept_next_alert:
-            alert.accept()
-        else:
-            alert.dismiss()
+        # select first contact for edit
+        wd.find_element_by_xpath("//img[@alt='Edit']").click()
+        # edit contact
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.first_name)
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.last_name)
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(contact.address)
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(contact.home_phone_number)
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(contact.mobile_phone_number)
+        wd.find_element_by_name("work").click()
+        wd.find_element_by_name("work").clear()
+        wd.find_element_by_name("work").send_keys(contact.work_phone_number)
+        wd.find_element_by_name("fax").click()
+        wd.find_element_by_name("fax").clear()
+        wd.find_element_by_name("fax").send_keys(contact.fax_number)
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(contact.email_1)
+        wd.find_element_by_name("email2").click()
+        wd.find_element_by_name("email2").clear()
+        wd.find_element_by_name("email2").send_keys(contact.email_2)
+        wd.find_element_by_name("email3").click()
+        wd.find_element_by_name("email3").clear()
+        wd.find_element_by_name("email3").send_keys(contact.email_3)
+        wd.find_element_by_xpath("(//input[@name='update'])[2]").click()
+        self.app.return_to_home_page()
--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -3,9 +3,9 @@ def test_delete_first_contact(app):
     app.contact.delete_first_contact()
     app.session.logout()
 
-
-def test_delete_all_contact(app):
-    app.session.login(username="admin", password="secret")
-    app.contact.delete_all_contact()
-    app.session.logout()
+# !!! it work but you should run it last!!!
+# def test_delete_all_contact(app):
+#     app.session.login(username="admin", password="secret")
+#     app.contact.delete_all_contact()
+#     app.session.logout()
 
