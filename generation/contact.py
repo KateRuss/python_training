9bd62415756c94eeae89c7722791f48e54998046
Екатерина <erusetskaya@gmail.com>
2020-04-17 19:18:28 +0300

    добавлен генератор тестовых данных для контактов (..\generation\contact.py) - правки

--- generation/contact.py
+++ generation/contact.py
@@ -7,7 +7,7 @@ import getopt
 import sys
 
 try:
-    opts, args = getopt.getopt(sys.argv[1:], "n:f:", ["number of groups", "file"])
+    opts, args = getopt.getopt(sys.argv[1:], "n:f:", ["number of contacts", "file"])
 except getopt.GetoptError as err:
     getopt.usage()
     sys.exit(2)
@@ -26,11 +26,11 @@ def random_string(prefix, maxlen):
     return prefix + "".join((random.choice(symbols) for i in range(random.randrange(maxlen))))
 
 
-testdata = [Contact(first_name=random_string("first_Name:", 20), last_name=random_string("Last_name:", 20),
+testdata = [Contact(first_name="", last_name="")]+[Contact(first_name=random_string("first_Name:", 20), last_name=random_string("Last_name:", 20),
                     address=random_string("address:", 20), home_phone_number=random_string("H:", 20),
                     mobile_phone_number=random_string("M:", 20), work_phone_number=random_string("W:", 20),
                     email_1=random_string("E1:", 25), email_2=random_string("E2:", 25), email_3=random_string("E3:", 10))
-    for i in range(5)
+    for i in range(n)
 ]
 
 file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
