50870fbb04d81d69c908837ea26fbb94ae9e9779
Екатерина <erusetskaya@gmail.com>
2020-04-21 21:30:15 +0300

    отключаемые проверки загрузки тестовых данных из ui

--- fixtura/group.py
+++ fixtura/group.py
@@ -54,6 +54,15 @@ class GroupHelper:
         self.return_to_groups_page()
         self.group_cache = None
 
+    def delete_group_by_id(self, id):
+        wd = self.app.wd
+        self.open_groups_page()
+        self.select_group_by_id(id)
+        #submit deleted
+        wd.find_element_by_name("delete").click()
+        self.return_to_groups_page()
+        self.group_cache = None
+
     def edit_first_group(self, group):
         wd = self.app.wd
         self.open_groups_page()
@@ -72,6 +81,10 @@ class GroupHelper:
     def select_group_by_index(self, index):
         wd = self.app.wd
         wd.find_elements_by_name("selected[]")[index].click()
+
+    def select_group_by_id(self, id):
+        wd = self.app.wd
+        wd.find_element_by_css_selector("input[value='%s']" % id).click()
 
     def modify_group_by_index(self, index, new_group_data):
         wd = self.app.wd
